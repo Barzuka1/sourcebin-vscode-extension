d299099c32c2b626fc249752ecf55ba4a01d50ad
Barzuka <67840224+Barzuka1@users.noreply.github.com>
2020-12-18 17:59:03 +0200

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 const vscode = require('vscode')
 const sourcebin = require('sourcebin')
+const { default : axios } = require('axios')
 const clipboardy = require('clipboardy');
+const fs = require('fs')
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -8,8 +10,7 @@ const clipboardy = require('clipboardy');
 
 function activate(context) {
 	console.log('Extension is active')
-	let disposable = vscode.commands.registerCommand('sourcebinupload.createBin', function () {
-
+	let makeBin = vscode.commands.registerCommand('sourcebinupload.makeBin', () => {
 		let content
 
 		var editor = vscode.window.activeTextEditor
@@ -22,6 +23,7 @@ function activate(context) {
 
 		let copyLink = 'Copy Link'
 		let openLink = 'Open Link'
+		let copyCode = 'Copy Code'
 
 		let index = 0
 		editor.document.fileName.split('\\').forEach(obj => {
@@ -40,26 +42,41 @@ function activate(context) {
 			title: fileName,
 			description: 'Created with SourcebinUpload (vscode extension)'
 		})
-		.then(data => vscode.window.showInformationMessage("Bin created.", copyLink, openLink).then(s => {
+		.then(data => vscode.window.showInformationMessage("Bin created.", copyLink, openLink, copyCode).then(s => {
 			if (s === copyLink) {
 				clipboardy.writeSync(data.short)
 			} else if (s === openLink) {
 				vscode.env.openExternal(vscode.Uri.parse(data.short))
+			} else if (s === copyCode) {
+				clipboardy.writeSync(data.key)
 			}
 		}))
 		.catch(err => vscode.window.showErrorMessage(err))
 
 	})
 
-	context.subscriptions.push(disposable)
+	context.subscriptions.push(makeBin)
+
+	let saveBin = vscode.commands.registerCommand('sourcebinupload.saveBin', async ()  =>  {
+		const { activeTextEditor: editor } = vscode.window
+		const currentFolder = editor.document.fileName.split('\\').slice(0,-1).join('\\')
+		let code = await vscode.window.showInputBox()
+			axios.get(`https://sourceb.in/raw/${code}/0`).then(res => {
+				console.log(res.data)
+				console.log(currentFolder)
+				fs.writeFile(`${currentFolder}\\${code}`, res.data, console.error)
+				vscode.window.showInformationMessage(`Bin saved as ${code}`)
+		}).catch(err => vscode.window.showErrorMessage("Cannot find bin, ", err))
+	})
+
+	context.subscriptions.push(saveBin)
 }
 // @ts-ignore
 exports.activate = activate
 
 function deactivate() {}
 
-module.exports = {
+
 	// @ts-ignore
 	activate,
 	deactivate
-}
